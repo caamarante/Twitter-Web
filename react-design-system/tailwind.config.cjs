ab5170fac8ea311b3c5e55e1f1bba69d389d9f4e
Ana Carolina <ana.amarante.pb@compasso.com.br>
2022-11-10 09:41:11 -0300

    Adicionando as configurações que serão usadas no tailwind, como cores, fontes e tamanho de texto

--- react-design-system/tailwind.config.cjs
+++ react-design-system/tailwind.config.cjs
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.tsx',
+  ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: 13,
+      sm: 14,
+      md: 15, 
+      lg: 16,
+      xl: 19,
+      '2xl': 20,
+    },
+    colors: {
+      'black': '#0F1419',
+      'dark-100': '#17202A',
+      'dark-200': '#1C2733',
+      'dark-300': '#283340',
+      'dark-400': '#3A444C',
+      'dark-500': '#5B7083',
+      'dark-600': '#8899A6',
+      'dark-700': '#EBEEF0',
+      'dark-800': '#F7F9FA',
+      'primary-blue': '#1DA1F2',
+      'primary-blue-opacity': '#1DA1F280',
+      'dark-blue': '#0073BF',
+      'white': '#FFFFFF',
+      'transparent': 'transparent',
+    },
+    extend: {
+      fontFamily : { 
+        sans: 'Poppins, sans-serif'
+      },
+    },
   },
   plugins: [],
 }
